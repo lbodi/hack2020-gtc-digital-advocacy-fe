7ef31839f75159c35d758972818d192975a3f5fe
László Bódi <lazlo.bodi@logmein.com>
2020-07-07 17:29:11 +0200

    Updates

---
+++ precache-manifest.8d41c41bb9456664688e5f58bc6f059e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827369a53fd1081ea1feff684e4248f1",
+    "url": "/hack2020-gtc-digital-advocacy-fe/index.html"
+  },
+  {
+    "revision": "ddc16e952ca3239351d4",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/css/main.53185910.chunk.css"
+  },
+  {
+    "revision": "96b1721c3b92889eecef",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/js/2.4da9b2f2.chunk.js"
+  },
+  {
+    "revision": "2aff8cf35e0d5ba1e4add0f1b477519f",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/js/2.4da9b2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc16e952ca3239351d4",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/js/main.a4e8c5c1.chunk.js"
+  },
+  {
+    "revision": "373f5ca436409fc8cef2",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/js/runtime-main.37f861d1.js"
+  },
+  {
+    "revision": "84239ffbc7bd1857569ea926594b5596",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/media/campaign-cover.84239ffb.jpg"
+  },
+  {
+    "revision": "bc5875d89317a228948ae2301a509d51",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/media/deepak_doe.bc5875d8.jpg"
+  },
+  {
+    "revision": "374534f1551e81c5bdac823ac51b44d9",
+    "url": "/hack2020-gtc-digital-advocacy-fe/static/media/jennifer_doe.374534f1.jpg"
+  }
+]);
