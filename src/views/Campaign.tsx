46020f3f96df463cd54ce4549efcc6dbf4da4a09
László Bódi <lazlo.bodi@logmein.com>
2020-07-08 00:02:59 +0200

    media query fix

--- src/views/Campaign.tsx
+++ src/views/Campaign.tsx
@@ -4,15 +4,15 @@ import {CampaignRoutes} from "./CampaignRoutes";
 import {Image, Flex} from "@chakra-ui/core";
 
 export const Campaign = () => (
-    <Flex h="100%" direction={["column", "row"]}>
-        <Flex h={["50%", "100%"]} w={["100%", "50%"]}>
+    <Flex h="100%" direction={{base:"column", md:"row"}}>
+        <Flex h={{base: "50%", md: "100%"}} w={{base: "100%", md: "50%"}}>
             <Image src={campaigns[0].cover}
                    alt="Campaign cover"
                    objectFit="cover"
                    h="100%"
             />
         </Flex>
-        <Flex h={["50%", "100%"]} w={["100%", "50%"]}
+        <Flex h={{base: "50%", md: "100%"}} w={{base: "100%", md: "50%"}}
               padding="48px" boxSizing="border-box">
             <CampaignRoutes/>
         </Flex>
