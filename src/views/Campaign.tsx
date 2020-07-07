b71ee35307df3d7f7fedc523afea9c6a3a9cb207
László Bódi <lazlo.bodi@logmein.com>
2020-07-07 23:15:41 +0200

    added mediaquery

--- src/views/Campaign.tsx
+++ src/views/Campaign.tsx
@@ -4,15 +4,16 @@ import {CampaignRoutes} from "./CampaignRoutes";
 import {Image, Flex} from "@chakra-ui/core";
 
 export const Campaign = () => (
-    <Flex h="100%">
-        <Flex w="50%" h="100%">
+    <Flex h="100%" direction={["column", "row"]}>
+        <Flex h={["50%", "100%"]} w={["100%", "50%"]}>
             <Image src={campaigns[0].cover}
                    alt="Campaign cover"
                    objectFit="cover"
                    h="100%"
             />
         </Flex>
-        <Flex w="50%" h="100%" padding="48px" boxSizing="border-box">
+        <Flex h={["50%", "100%"]} w={["100%", "50%"]}
+              padding="48px" boxSizing="border-box">
             <CampaignRoutes/>
         </Flex>
     </Flex>
